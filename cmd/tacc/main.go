7ba51da86c92735daa483b9453e0ddb4cb6996ff
Juan Cespedes <juan.cespedes@imdea.org>
2019-09-12 20:46:45 +0200

    minor

--- cmd/tacc/main.go
+++ cmd/tacc/main.go
@@ -27,19 +27,36 @@ func main() {
 	table.SetSeparator(tview.Borders.Vertical)
 	table.SetFixed(1, 0)
 	table.SetSelectable(true, false)
-	table.SetCell(0, 0, tview.NewTableCell("[yellow]date").SetSelectable(false))
-	table.SetCell(0, 1, tview.NewTableCell("[yellow]concept").SetSelectable(false).SetExpansion(1))
-	table.SetCell(0, 2, tview.NewTableCell("[yellow]value").SetSelectable(false))
-	table.SetCell(0, 3, tview.NewTableCell("[yellow]balance").SetSelectable(false))
+	table.SetCell(0, 0, tview.NewTableCell("[yellow]date").
+		SetSelectable(false).
+		SetAlign(tview.AlignCenter).
+		SetMaxWidth(10))
+	table.SetCell(0, 1, tview.NewTableCell("[yellow]concept").
+		SetSelectable(false).
+		SetAlign(tview.AlignCenter).
+		SetExpansion(1))
+	table.SetCell(0, 2, tview.NewTableCell("[yellow]value").
+		SetSelectable(false).
+		SetAlign(tview.AlignCenter).
+		SetMaxWidth(10))
+	table.SetCell(0, 3, tview.NewTableCell("[yellow]balance").
+		SetSelectable(false).
+		SetAlign(tview.AlignCenter).
+		SetMaxWidth(11))
 	for i, t := range transactions {
 		table.SetCell(i+1, 0, tview.NewTableCell(t.Time.Format("2006-01-02")))
 		table.SetCell(i+1, 1, tview.NewTableCell(t.Description))
+		table.SetCell(i+1, 2, tview.NewTableCell(fmt.Sprintf("%d.%02d", 10*i, i%100)).SetAlign(tview.AlignRight))
+		table.SetCell(i+1, 3, tview.NewTableCell("123456.78"))
 	}
 	app.SetRoot(table, true)
 	app.SetBeforeDrawFunc(func(screen tcell.Screen) bool {
-		cell := table.GetCell(1, 1)
-		_, _, width, _ := table.GetRect()
-		cell.SetText(fmt.Sprintf("width = %d", width))
+		/*
+			_, _, width, _ := table.GetRect()
+			for i := 1; i < table.GetRowCount(); i++ {
+				table.GetCell(i, 1).SetMaxWidth(width - 33)
+			}
+		*/
 		return false
 	})
 	if err := app.Run(); err != nil {
