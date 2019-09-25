2254a9d57d165074a0cc90e9eb89ab301c5b0541
Juan Cespedes <cespedes@thehackers.org>
2019-09-25 23:09:48 +0200

    cmd/tacc: switch to github.com/cespedes/tableview

--- cmd/tacc/main.go
+++ cmd/tacc/main.go
@@ -5,8 +5,7 @@ import (
 	"os"
 
 	"github.com/cespedes/accounting"
-	"github.com/gdamore/tcell"
-	"github.com/rivo/tview"
+	"github.com/cespedes/tableview"
 
 	_ "github.com/cespedes/accounting/backend/postgres"
 )
@@ -20,50 +19,19 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("ledger = %#v\n", ledger)
 
 	// accounts := ledger.Accounts()
 	transactions := ledger.Transactions()
 
-	app := tview.NewApplication()
-	table := tview.NewTable()
-	table.SetBorder(false)
-	table.SetTitle(" Accounting ")
-	table.SetSeparator(tview.Borders.Vertical)
-	table.SetFixed(1, 0)
-	table.SetFixedColumnsWidth(true)
-	table.SetSelectable(true, false)
-	table.SetCell(0, 0, tview.NewTableCell("[yellow]date").
-		SetSelectable(false).
-		SetAlign(tview.AlignCenter).
-		SetMaxWidth(10))
-	table.SetCell(0, 1, tview.NewTableCell("[yellow]concept").
-		SetSelectable(false).
-		SetAlign(tview.AlignCenter).
-		SetExpansion(1))
-	table.SetCell(0, 2, tview.NewTableCell("[yellow]value").
-		SetSelectable(false).
-		SetAlign(tview.AlignCenter).
-		SetMaxWidth(10))
-	table.SetCell(0, 3, tview.NewTableCell("[yellow]balance").
-		SetSelectable(false).
-		SetAlign(tview.AlignCenter).
-		SetMaxWidth(11))
-	for i, t := range transactions {
-		table.SetCell(i+1, 0, tview.NewTableCell(t.Time.Format("2006-01-02")))
-		table.SetCell(i+1, 1, tview.NewTableCell(t.Description))
-		table.SetCell(i+1, 2, tview.NewTableCell(fmt.Sprintf("%d.%02d", 10*i, i%100)).SetAlign(tview.AlignRight))
-		table.SetCell(i+1, 3, tview.NewTableCell("123456.78"))
+	t := tableview.NewTableView()
+	t.FillTable([]string{"date", "concept", "value", "balance"}, [][]string{})
+	t.SetExpansion(1, 1)
+
+	for i, tr := range transactions {
+		t.SetCell(i, 0, tr.Time.Format("2006-01-02"))
+		t.SetCell(i, 1, tr.Description)
+		t.SetCell(i, 2, fmt.Sprintf("%d", len(tr.Splits)))
+		t.SetCell(i, 3, "123456.78")
 	}
-	app.SetRoot(table, true)
-	app.SetBeforeDrawFunc(func(screen tcell.Screen) bool {
-		_, _, width, _ := table.GetRect()
-		for i := 1; i < table.GetRowCount(); i++ {
-			table.GetCell(i, 1).SetMaxWidth(width - 33)
-		}
-		return false
-	})
-	if err := app.Run(); err != nil {
-		panic(err)
-	}
+	t.Run()
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module github.com/cespedes/accounting
 
 require (
-	github.com/gdamore/tcell v1.1.2
+	github.com/cespedes/tableview v0.0.0-20190925210630-8bbfb4221325
 	github.com/lib/pq v1.2.0
-	github.com/rivo/tview v0.0.0-20190829161255-f8bc69b90341
+	golang.org/x/sys v0.0.0-20190924154521-2837fb4f24fe // indirect
 )
 
 replace github.com/rivo/tview => github.com/cespedes/tview v0.0.0-20190912222215-e6745533fb9c
