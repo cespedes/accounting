66cd43ad9e13f60a992bb2d109c8fffd914ae511
Juan Cespedes <juan.cespedes@imdea.org>
2020-02-18 23:15:34 +0100

    added backend/ledger/parser.go

---
+++ backend/ledger/parser.go
@@ -0,0 +1,144 @@
+package ledger
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+/* Syntax of ledger files using EBNF:
+
+line    = ( directive | transaction_line | split_line ) .
+directive = ( include_line | account_line | price_line | default_currency_line | commodity_line ) .
+
+letter = unicode_letter .
+digit  = "0" … "9" .
+digits = digit { digit } .
+punct = "." | "," | " " | "_" .
+currency_char = letter | digit | "$" | "/" | "_" | "." .
+currency = currency_char { currency_char } .
+integer = ( digit { digit} ) | ( digit [ digit [ digit ] ] { punct digit digit digit } ) .
+number = [ "-" ] integer [ punct digit { digit } ]
+value = ( currency number ) | ( currency " " number ) | ( number currency ) | (number " " currency ) .
+date = digit digit digit digit ( "-" | "/" ) digit digit ( "-" | "/" ) digit digit
+indent = " " { " " }
+transaction_price = ( "@" | "@@" ) value .
+balance_assertion = ( "=" | "=*" | "==" | "==*" ) value [ transaction_price ] .
+
+include_line = "include" filename .
+price_line   = "P" date currency value .
+default_currency_line = "D" [ currency | value ] .
+transaction_line = date description .
+split_line = indent account_name [ two_spaces [ value [ transaction_price ] ] [ balance_assertion ] ] .
+commodity_line = "commodity" value .
+account_name = ( letter | digit ) { letter | digit | ":" | " " } .
+account_line = "account" account_name
+
+*/
+
+type scannerFile struct {
+	f        *os.File
+	s        *bufio.Scanner
+	filename string
+	lineNum  int
+}
+
+type Scanner struct {
+	files []scannerFile
+}
+
+type ScannerLine struct {
+	Filename string
+	LineNum  int
+	Text     string
+	Err      error
+}
+
+func NewScanner() *Scanner {
+	s := new(Scanner)
+	return s
+}
+
+func (s *Scanner) NewFile(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	s2 := bufio.NewScanner(f)
+	s.files = append(s.files, scannerFile{f: f, s: s2, filename: filename})
+	return nil
+}
+
+func (s *Scanner) Line() ScannerLine {
+	if len(s.files) == 0 {
+		return ScannerLine{Err: io.EOF}
+	}
+	var line ScannerLine
+	file := s.files[len(s.files)-1]
+	line.Filename = file.filename
+	line.LineNum = file.lineNum
+	if file.s.Scan() {
+		s.files[len(s.files)-1].lineNum++
+		line.LineNum++
+		line.Text = file.s.Text()
+		return line
+	}
+	line.Err = file.s.Err()
+	if line.Err == nil {
+		s.files = s.files[:len(s.files)-1]
+		return s.Line()
+	}
+	return line
+}
+
+// ReadFile fills a ledger with the data from a journal file.
+func (l ledger) Read() error {
+	s := NewScanner()
+	s.NewFile(l.file)
+
+	for {
+		line := s.Line()
+		if line.Err != nil {
+			if line.Err != io.EOF {
+				return line.Err
+			}
+			return nil
+		}
+		fmt.Printf("%s:%d: \"%s\"\n", line.Filename, line.LineNum, line.Text)
+		if strings.HasPrefix(line.Text, "include ") {
+			pieces := strings.Split(line.Text, " ")
+			if len(pieces) >= 2 {
+				err := s.NewFile(pieces[1])
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+}
+
+func ReadFile(filename string) {
+	s := NewScanner()
+	s.NewFile(filename)
+	for {
+		line := s.Line()
+		if line.Err != nil {
+			if line.Err != io.EOF {
+				panic(line.Err)
+			}
+			break
+		}
+		fmt.Printf("%s:%d: \"%s\"\n", line.Filename, line.LineNum, line.Text)
+		if strings.HasPrefix(line.Text, "include ") {
+			pieces := strings.Split(line.Text, " ")
+			if len(pieces) >= 2 {
+				err := s.NewFile(pieces[1])
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+}
