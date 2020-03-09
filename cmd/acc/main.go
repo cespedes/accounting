631d3454841f93af700b07279a6c20cc49a2da75
Juan Cespedes <cespedes@thehackers.org>
2020-03-09 16:35:10 +0100

    added simple test program

---
+++ cmd/acc/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/cespedes/accounting"
+	"github.com/cespedes/accounting/backend/ledger"
+	_ "github.com/cespedes/accounting/backend/postgres"
+	_ "github.com/cespedes/accounting/backend/txtdb"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "Usage: tacc <database>")
+		os.Exit(1)
+	}
+	L, err := accounting.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	ledger.Display(os.Stdout, L)
+	//	for _, a := range L.Accounts {
+	//		fmt.Printf("%#v\n", a)
+	//	}
+}
